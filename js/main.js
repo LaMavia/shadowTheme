5df91e45b4018d144994fc2c9d45e374eef4e023
LastShadowPL <xeloxpl@windowslive.com>
2017-08-24 11:26:08 +0200

    doint something

--- js/main.js
+++ js/main.js
@@ -10,6 +10,15 @@
 
 //Events
 const menuItems = document.querySelector('nav.menu').children;
-menuItems[menuItems.length - 1].addEventListener('click', (e) => {
-    e.target.classList.toggle('active');
-}, true);
+window.addEventListener('resize', (e) => {
+    if(window.innerWidth > 1366){
+        document.querySelector('nav.menu').classList.add('hid');
+        menuItems[menuItems.length - 1].addEventListener('click', (e) => {
+            document.querySelector('nav.menu').classList.toggle('hid');
+            e.target.classList.toggle('active');
+        }, true);
+    }else if(window.innerWidth < 1366 && window.innerWidth > 1025){
+        document.querySelector('nav.menu').classList.remove('hid');
+        menuItems[menuItems.length - 1].removeEventListener('click');
+    }
+})
